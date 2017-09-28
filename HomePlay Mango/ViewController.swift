54314bfa2e5806320c199d7974d132bec7530e09
Vansa Pha <vs.lov.rs@gmail.com>
2017-09-28 11:34:29 +0700

    init dev brand

--- HomePlay Mango/ViewController.swift
+++ HomePlay Mango/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //load new brand from vmWare, dev
     }
 
     override func didReceiveMemoryWarning() {
